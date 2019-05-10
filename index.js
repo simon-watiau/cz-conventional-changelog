6dac73717e1ccf3e5c2a6ef3701443d1d25d5f7b
Simon Watiau <watiau.simon@gmail.com>
2019-05-10 10:52:17 +0200

    feat(titles): Add `temp` title
    Add `temp` title as a marker for intermediate commits to be squashed
    
    branch: master

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 
 var engine = require('./engine');
 var conventionalCommitTypes = require('conventional-commit-types');
+var types = conventionalCommitTypes.types;
+
+types['temp'] = {
+  'description': 'An intermediate commit to be squashed',
+  'title': 'intermediate commit'
+};
 
 module.exports = engine({
-  types: conventionalCommitTypes.types,
+  types: types,
   defaultType: process.env.CZ_TYPE,
   defaultScope: process.env.CZ_SCOPE,
   defaultSubject: process.env.CZ_SUBJECT,
